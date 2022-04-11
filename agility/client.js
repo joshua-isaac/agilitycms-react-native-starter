32e5a7f0437eacc1c8e62af029cdb670ca8ef3eb
joshua <hello.joshua@hotmail.com>
2022-04-11 10:37:15 -0400

    updates

--- agility/client.js
+++ agility/client.js
@@ -6,7 +6,7 @@ import {
   } from "@apollo/client";
 
   export const client = new ApolloClient({
-    uri: 'https://api.aglty.io/v1/5879e0cc-u/preview/en-us/graphql',
+    uri: `https://api.aglty.io/v1/5879e0cc-u/preview/en-us/graphql`,
     cache: new InMemoryCache(),
     headers: {
       apiKey: config.AGILITY_API_KEY
